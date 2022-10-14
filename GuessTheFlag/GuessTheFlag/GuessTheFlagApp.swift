e55141a87d61a0b3f1650c2a5e0141f8599a5040
Abdurokhman Mukhanbetov <muhanbetov07@icloud.com>
2022-10-15 00:51:40 +0400

    Add files via upload

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Abdurokhman Mukhanbetov on 07.09.22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
